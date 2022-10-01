e01faebcb0efc1f30302f20811ad87b64df6d1fd
Luiz Felipe Pedrosa Cavalcante <54713427+gitBiohazard@users.noreply.github.com>
2022-10-01 01:02:04 -0300

    upload

---
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Acoes } from './modelo/acoes';
+import { AcoesService } from './acoes.service';
+import { Subscription, merge } from 'rxjs';
+import {
+  tap,
+  switchMap,
+  filter,
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs/operators';
+
+const ESPERA_DIGITACAO = 300;
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css'],
+})
+export class AcoesComponent {
+  acoesInput = new FormControl();
+  todasAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => {
+      console.log('Fluxo Inicial');
+    })
+  );
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
+    tap(() => {
+      console.log('Fluxo do Filtro');
+    }),
+    tap(console.log),
+    filter(
+      (valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length
+    ),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+    tap(console.log)
+  );
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
+
+  constructor(private acoesService: AcoesService) {}
+}
---
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, pluck } from 'rxjs/operators';
+import { Acao, AcoesAPI } from './modelo/acoes';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AcoesService {
+  constructor(private httpClient: HttpClient) {}
+
+  getAcoes(valor?: string) {
+    const params = valor ? new HttpParams().append('valor', valor) : undefined;
+    return this.httpClient
+      .get<AcoesAPI>('http://localhost:3000/acoes', { params })
+      .pipe(
+        tap((valor) => console.log(valor)),
+        pluck('payload'),
+        map((acoes) =>
+          acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))
+        )
+      );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+
+    if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+
+    return 0;
+  }
+}
